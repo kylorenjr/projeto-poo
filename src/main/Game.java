419d3f36bea98be1fd78cab517fadfb8dc88bf83
joaog212 <joaoglmbarbosa@gmail.com>
2025-10-31 17:42:58 -0300

    criação da pasta de resources do projeto, alterações em ambas as classes para carregamento da imagem e das sprites

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,22 +1,39 @@
 package main;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame; //*=Jframe
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame {
 
     private GameScreen gameScreen; //criamos um novo objeto GameScreen para podermos usar
 
-    public Game(){
+    private BufferedImage img; //criação do objeto imagem
+
+    public Game() throws IOException {
+
+        importImg(); //metodo que permite a exibição da imagem na tela
 
         setSize(600, 600); //tamanho da janela
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE); //oq acontece por padrão quando fechamos a janela, nesse caso encerra o game
         setLocationRelativeTo(null); //diz onde a janela vai aparecer quando iniciar o game
-        gameScreen = new GameScreen(); // aqui inicializamos o objeto GameScreen
+        gameScreen = new GameScreen(img); // aqui inicializamos o objeto GameScreen
         //Jframe.add(Jpanel);
         add(gameScreen);
     }
 
-    public static void main(String[] args){
+    private void importImg() throws IOException {
+
+        InputStream is = getClass().getResourceAsStream("/AtlasSprites.png");
+
+        img = ImageIO.read(is);
+
+        //IOExeception é necessário para evitar bugs ao carregar imagem e não causar problemas maiores se não conseguir achar a imagem
+    }
+
+    public static void main(String[] args) throws IOException {
         Game game = new Game();
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -3,10 +3,36 @@ package main;
 import javax.swing.JPanel; //jpanel é um container?
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.image.BufferedImage; //importando classe para conseguir usar a imagem
+import java.util.ArrayList;
+import java.util.Random; //importando a classe random
 
 public class GameScreen extends JPanel{
 
-    public GameScreen(){
+    private Random random; //criação do objeto random
+    private BufferedImage img; //criação do objeto img
+
+    private ArrayList<BufferedImage> sprites = new ArrayList<>(); //inicializando um array list para carregar todas as sprites que vão ser usadas no jogo
+
+    public GameScreen(BufferedImage img){
+        this.img = img; //inicializando a imagem
+        
+        loadSprites();
+        
+        random = new Random(); //inicializando variavel random
+
+    }
+
+    private void loadSprites() {
+
+        //este metodo carrega todas as sprites (32x32) da imagem (320x320) para facilitar a impressão delas na tela
+        //y e x < 10 é porque como a imagem é 320x320 e as sprites são 32x32 teriamos um eixo X e Y de 10 (ja que 320/32 = 10)
+
+        for(int y = 0; y < 10; y++) {
+            for(int x = 0; x < 10; x++) {
+                sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+            }
+        }
 
     }
 
@@ -14,7 +40,21 @@ public class GameScreen extends JPanel{
         super.paintComponent(g);//delega para o JPanel as configurações de vídeo
 
         //g.drawRect(50, 50, 120, 100);
-        g.setColor(Color.BLUE);
-        g.fillRect(50, 50, 120, 100);
+        //g.setColor(Color.BLUE);
+        //g.fillRect(50, 50, 120, 100);
+
+        //g.drawImage(img, 0, 0, null);
+
+        //este nested loop é apenas para confirmar que a imagem foi carregada e que os sprites foram armazenados no array list criado acima, não será mantido no jogo, é apenas um teste
+        for(int y = 0; y < 20; y++) {
+            for(int x = 0; x < 20; x++) {
+                g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
+            }
+        }
+
+    }
+
+    private int getRndInt() {
+        return random.nextInt(100);
     }
 }
