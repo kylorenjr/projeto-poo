f1ffda5293f53d41d0ba3330f8c5bdb992b88109
Lucas M Ventura <lmventura311205@gmail.com>
2025-11-05 00:51:28 -0300

    Adiciona classe de UI MyButton e implementa no Menu

--- src/scenes/Menu.java
+++ src/scenes/Menu.java
@@ -1,36 +1,59 @@
 package scenes;
 
-import main.Game;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+import main.Game;
+import ui.MyButton;
+import static main.GameStates.*;
+
 public class Menu extends GameScene implements SceneMethods {
 
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
     private BufferedImage img;
-    private Random random;
+    //private Random random;
+
+    private MyButton bPlaying, bSettings, bQuit;
+
 
     public Menu(Game game) throws IOException {
         super(game);
-        random = new Random();
+        //random = new Random();
         importImg();
         loadSprites();
+        initButtons();
+    }
+
+    private void initButtons() {
+
+        int w = 150;
+        int h = w / 3;
+        int x = 640 / 2 - w / 2;
+        int y = 150;
+        int yOffset = 100;
+
+        bPlaying = new MyButton("Jogar", x, y, w, h);
+        bSettings = new MyButton("Configurações", x, y + yOffset, w, h);
+        bQuit = new MyButton("Sair", x, y + yOffset * 2, w, h);
+
     }
 
     @Override
     public void render(Graphics g) {
-        //codigo atual serve apenas para testes
-        for(int y = 0; y < 20; y++) {
-            for(int x = 0; x < 20; x++) {
-                g.drawImage(sprites.get(getRndInt()), x * 32, y * 32, null);
-            }
-        }
+
+        drawButtons(g);
+    }
+
+    private void drawButtons(Graphics g) {
+        bPlaying.draw(g);
+        bSettings.draw(g);
+        bQuit.draw(g);
+
     }
 
     private void loadSprites() {
@@ -43,16 +66,64 @@ public class Menu extends GameScene implements SceneMethods {
 
     }
 
-    private int getRndInt() {
-        return random.nextInt(100);
-    }
-
     private void importImg() throws IOException {
 
         InputStream is = getClass().getResourceAsStream("/AtlasSprites.png");
 
         img = ImageIO.read(is);
 
-        //IOExeception é necessário para evitar bugs ao carregar imagem e não causar problemas maiores se não conseguir achar a imagem
+    }
+
+    //private int getRndInt() {
+        //return random.nextInt(100);
+    //}
+
+    @Override
+    public void mouseClicked(int x, int y) {
+
+        if (bPlaying.getBounds().contains(x, y)) {
+            SetGameState(PLAYING);
+        } else if (bSettings.getBounds().contains(x, y)) {
+            SetGameState(SETTINGS);
+        } else if (bQuit.getBounds().contains(x, y))
+            System.exit(0);
+    }
+
+    @Override
+    public void mouseMoved(int x, int y) {
+        bPlaying.setMouseOver(false);
+        bSettings.setMouseOver(false);
+        bQuit.setMouseOver(false);
+
+        if (bPlaying.getBounds().contains(x, y)) {
+            bPlaying.setMouseOver(true);
+        } else if (bSettings.getBounds().contains(x, y)) {
+            bSettings.setMouseOver(true);
+        } else if (bQuit.getBounds().contains(x, y)) {
+            bQuit.setMouseOver(true);
+        }
+    }
+
+    @Override
+    public void mousePressed(int x, int y) {
+
+        if (bPlaying.getBounds().contains(x, y)) {
+            bPlaying.setMousePressed(true);
+        } else if (bSettings.getBounds().contains(x, y)) {
+            bSettings.setMousePressed(true);
+        } else if (bQuit.getBounds().contains(x, y)) {
+            bQuit.setMousePressed(true);
+        }
+    }
+
+
+    @Override
+    public void mouseReleased(int x, int y) {resetButtons();}
+
+    private void resetButtons() {
+    bPlaying.resetBooleans();
+    bSettings.resetBooleans();
+    bQuit.resetBooleans();
     }
 }
+
